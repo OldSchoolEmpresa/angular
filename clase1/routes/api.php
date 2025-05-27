26abcccb127508734bdbd7a63940ba90be50a72f
oldschoolempresa <posadaalberto48@gmail.com>
2025-05-26 21:16:06 -0500

    se agrego correctamente

--- clase1/routes/api.php
+++ clase1/routes/api.php
@@ -5,20 +5,21 @@ use App\Http\controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PedidoController; // Nuevo controlador
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
 use App\Http\Controllers\LoginController;
 
 
-Route::get('/empleado', [empleadocontrolador::class, 'index']);
-Route::post('/empleado', [empleadoControlador::class, 'store']);
-Route::get('/empleado/{idEmpleado}', [empleadocontrolador::class, 'show']);
-Route::post('/empleado', [empleadocontrolador::class, 'store']);
-Route::put('/empleado/{idEmpleado}', [empleadocontrolador::class, 'update']);
-Route::delete('/empleado/{idEmpleado}', [empleadocontrolador::class, 'destroy']);
-Route::post('/login', [LoginController::class, 'login']);
-Route::post('/register', [EmpleadoController::class, 'register']);
+
+Route::get('/empleado', [Empleadocontrolador::class, 'index']);
+Route::post('/empleado', [EmpleadoControlador::class, 'store']);
+Route::get('/empleado/{idEmpleado}', [Empleadocontrolador::class, 'show']);
+Route::put('/empleado/{idEmpleado}', [Empleadocontrolador::class, 'update']);
+Route::delete('/empleado/{idEmpleado}', [Empleadocontrolador::class, 'destroy']);
 Route::post('/pedidos', [PedidoController::class, 'store']);
 Route::get('/pedidos', [PedidoController::class, 'index']);
 Route::get('/pedidos/{id}', [PedidoController::class, 'show']);
 Route::post('/pedidos', [PedidoController::class, 'store']);
 Route::post('/register', [AuthController::class, 'store']);
-
+Route::post('/login', [LoginController::class, 'login']);
